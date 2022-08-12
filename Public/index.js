8f382fc99d11a1ee6530d31e35a029ff028e04b0
malay44 <malay.p1@ahduni.edu.in>
2022-08-12 11:01:38 +0530

    added distortion to plane and also configured dat.gui

--- Public/index.js
+++ Public/index.js
@@ -1,6 +1,45 @@
-// document.querySelector(".hi-class").innerText='hello'
-
 import * as THREE from "https://unpkg.com/three@0.143.0/build/three.module.js";
+// import {OrbitControls} from "https://unpkg.com/three@0.143.0/examples/jsm/controls/OrbitControls.js";
+// import * as dat from "https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.9/dat.gui.js"
+import * as dat from "/dat.gui/build/dat.gui.module.js"
+
+
+//<------------------------------- gui starts ------------------------------->
+const gui = new dat.GUI()
+const world = {
+    plane:{
+        width: 10,
+        height: 10,
+        widthSegments: 10,
+        heightSegments: 10
+    }
+}
+gui.add(world.plane, "width", 1, 20).onChange(regeneratePlane)
+
+gui.add(world.plane, "height", 1, 20).onChange(regeneratePlane)
+
+gui.add(world.plane, "widthSegments", 1, 20).onChange(regeneratePlane)
+
+gui.add(world.plane, "heightSegments", 1, 20).onChange(regeneratePlane)
+
+function regeneratePlane() {
+  planeMesh.geometry.dispose()
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+    )
+
+  const {array} = planeMesh.geometry.attributes.position
+
+  for (let i = 2; i < array.length; i+=3) {
+    let yCoordinate = array[i]
+    array[i] = yCoordinate + Math.random()
+  }
+}
+
+//<------------------------------- gui ends ------------------------------->
 
 // scene
 var scene = new THREE.Scene();
@@ -12,37 +51,51 @@ var camera = new THREE.PerspectiveCamera(
   1000
 );
 // renderer
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 
-const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+//plane
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
 
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading
 });
 
 let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
+//plane distortion generator
+const {array} = planeMesh.geometry.attributes.position
+
+for (let i = 2; i < array.length; i+=3) {
+  let yCoordinate = array[i]
+  array[i] = yCoordinate + Math.random()
+}
+
+//light
 let Light = new THREE.DirectionalLight(0xffffff,1);
 Light.position.set(0,0,1)
 
+//scene
 scene.add(planeMesh, Light);
 
+//camera
 camera.position.z = 10;
 
+
+//animation
 function animate() {
   requestAnimationFrame(animate);
+
+  //renderd
   renderer.render(scene, camera);
-//   planeMesh.rotation.y += 0.01
+  //planeMesh.rotation.y += 0.01
 }
 
+//animation initialized
 animate();
 
-// renderer.setSize(sizes.width, sizes.height);
+//render send to html
 document.body.appendChild(renderer.domElement);
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
 app.use(express.static(__dirname + '/Public'));
+app.use('/build/', express.static(path.join(__dirname, '/node_modules/three/build')));
+app.use('/jsm/', express.static(path.join(__dirname, '/node_modules/three/examples/jsm')));
+app.use('/dat.gui/', express.static(path.join(__dirname, '/node_modules/dat.gui')))
 
 
 app.get('/', (req, res) => {
